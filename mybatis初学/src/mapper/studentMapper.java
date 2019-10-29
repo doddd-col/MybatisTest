cbbe82b46be3339d5e99d9292f1d582261654682
doddd-col <761123576@qq.com>
2019-10-29 10:53:06 +0800

    接口方式的动态代理CRUD

---
+++ mybatis初学/src/mapper/studentMapper.java
@@ -0,0 +1,18 @@
+package mapper;
+
+import entity.Student;
+
+import java.util.List;
+
+//接口名=namespace名 方法名=id值 参数=parametertype值 返回类型=resulttype
+public interface studentMapper {
+    void addstudent(Student student);
+
+    void updatestudent(Student student);
+
+    void deletestudent(int sno);
+
+    List<Student> querystudentall();
+
+    Student querystudentBySno(int sno);
+}
