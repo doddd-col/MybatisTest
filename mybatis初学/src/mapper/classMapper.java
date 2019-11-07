07065251927be30da1a1a6eea42ea2f4b035c25b
doddd-col <761123576@qq.com>
2019-11-07 17:51:11 +0800

    删除

--- mybatis初学/src/mapper/classMapper.java
+++
@@ -1,7 +0,0 @@
-package mapper;
-
-import entity.StudentClass;
-
-public interface classMapper {
-    StudentClass queryClass(int classid);
-}
--- mybatis初学/src/mapper/studentCard.java
+++
@@ -1,8 +0,0 @@
-package mapper;
-
-import entity.StudentCard;
-
-public interface studentCard {
-
-    StudentCard querystudentCard(int cardid);
-}
